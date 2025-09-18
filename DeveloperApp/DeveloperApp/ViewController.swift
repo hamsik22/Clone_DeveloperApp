97fa54972b028381a80559a99db607e67de6453f
HwangSukHyeon <payback9488@gmail.com>
2025-09-19 04:33:49 +0900

    [Proj]: 프로젝트 생성 및 코드베이스 세팅

---
+++ DeveloperApp/DeveloperApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeveloperApp
+//
+//  Created by 황석현 on 9/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DeveloperApp/DeveloperAppTests/DeveloperAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeveloperAppTests.swift
+//  DeveloperAppTests
+//
+//  Created by 황석현 on 9/19/25.
+//
+
+import Testing
+@testable import DeveloperApp
+
+struct DeveloperAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
