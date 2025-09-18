45e59ba056f3482a5113e5ea0ad9708ce0ffcc2d
HwangSukHyeon <payback9488@gmail.com>
2025-09-19 05:11:48 +0900

    [Add]: 탭바 설정 및 관련 뷰컨트롤러 추가

---
+++ DeveloperApp/DeveloperApp/Controllers/BrowseTabViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BrowseTabViewController.swift
+//  DeveloperApp
+//
+//  Created by 황석현 on 9/19/25.
+//
+
+import UIKit
+
+class BrowseTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .orange
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DeveloperApp/DeveloperApp/TabBarViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  DeveloperApp
+//
+//  Created by 황석현 on 9/19/25.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .black
+        let discoverTab = UINavigationController(rootViewController: DiscoverTabViewController())
+        let browseTab = UINavigationController(rootViewController: BrowseTabViewController())
+        let wwdcTab = UINavigationController(rootViewController: WWDCTabViewController())
+        let accountTab = UINavigationController(rootViewController: AccountTabViewController())
+        let searchTab = UINavigationController(rootViewController: SearchTabViewController())
+        
+        discoverTab.title = "Discover"
+        browseTab.title = "Browse"
+        wwdcTab.title = "WWDC"
+        accountTab.title = "Account"
+        searchTab.title = "Search"
+        
+        discoverTab.tabBarItem.image = UIImage(systemName: "star.fill")
+        browseTab.tabBarItem.image = UIImage(systemName: "rectangle.stack.fill")
+        wwdcTab.tabBarItem.image = UIImage(systemName: "apple.logo")
+        accountTab.tabBarItem.image = UIImage(systemName: "person.circle.fill")
+        searchTab.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        
+        setViewControllers([discoverTab, browseTab, wwdcTab, accountTab, searchTab], animated: true)
+        
+    }
+
+
+}
+
+#Preview {
+    TabBarViewController()
+}
